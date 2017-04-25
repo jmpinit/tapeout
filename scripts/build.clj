d05c7d17510b80d1c54145eee5a5c3642cc8e121
Owen Trueblood <hi@owentrueblood.com>
2017-04-25 00:47:22 -0400

    Enable sourcemaps

--- scripts/build.clj
+++ scripts/build.clj
@@ -5,9 +5,8 @@
 (let [start (System/nanoTime)]
   (b/build "src"
     {:main 'tapeout.core
+     :source-paths ["src"]
      :output-to "out/tapeout.js"
      :output-dir "out"
      :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
-
-
